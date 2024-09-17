b51d871a4e716932e38056c748b8902db038cea9
Adam Morsy <142569898+adam-morsy@users.noreply.github.com>
2024-09-17 13:53:23 -0400

    fixed post to project

--- src/js/cms.js
+++ src/js/cms.js
@@ -5,7 +5,7 @@ import CMS from "decap-cms-app";
 import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.scss";
 
 import HomePreview from "./cms-preview-templates/home";
-import PostPreview from "./cms-preview-templates/project";
+import ProjectPreview from "./cms-preview-templates/project";
 import ProductsPreview from "./cms-preview-templates/products";
 import ValuesPreview from "./cms-preview-templates/values";
 import ContactPreview from "./cms-preview-templates/contact";
@@ -13,7 +13,7 @@ import FooterPreview from "./cms-preview-templates/footer";
 
 CMS.registerPreviewStyle(styles, {raw: true});
 CMS.registerPreviewTemplate("home", HomePreview);
-CMS.registerPreviewTemplate("post", ProjectPreview);
+CMS.registerPreviewTemplate("project", ProjectPreview);
 CMS.registerPreviewTemplate("products", ProductsPreview);
 CMS.registerPreviewTemplate("values", ValuesPreview);
 CMS.registerPreviewTemplate("contact", ContactPreview);
